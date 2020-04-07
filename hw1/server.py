a343ceb4e5bc02382f15faa216a1734ea695df95
yutt026 <samuelyuyy@gmail.com>
2020-04-07 12:06:59 +0800

    Fix server

--- hw1/server.py
+++ hw1/server.py
@@ -1,16 +1,21 @@
+import sys
 import socket
 import threading
 from cthread import ClientThread
 
-PORT = 7811
+PORT = 7788
+
+if len(sys.argv) == 2:
+    PORT = int(sys.argv[1])
+
 host = socket.gethostname()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((host, PORT))
-print("Server started", PORT)
+print("Server started using port", PORT)
 
 while True:
     server.listen(5)
     c_socket, c_address = server.accept()
-    newthread = ClientThread(c_socket, c_address)
-    newthread.start()
+    new_connection = ClientThread(c_socket, c_address)
+    new_connection.start()
