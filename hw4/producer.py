d9152e3b0775f05e7ced48a617e9a0314693f175
samuelyutt <samuelyuyy@gmail.com>
2020-06-13 00:30:24 +0800

    Remove toy example files

--- hw4/producer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaProducer
-
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-future = producer.send('test', key=b'12345', value=b'abcde')
-result = future.get(timeout=10)
-print(result)
